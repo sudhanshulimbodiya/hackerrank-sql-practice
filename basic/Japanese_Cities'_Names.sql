500be410b914ee161e4547dc6a528147d61e486b
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 17:19:13 +0530

    Add SQL query for Japanese cities' names
    Add SQL query to select Japanese cities from the CITY table.

---
+++ basic/Japanese_Cities'_Names.sql
@@ -0,0 +1,8 @@
+-- Problem: Japanese Cities' Names
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-name/submissions/code/440565842
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
