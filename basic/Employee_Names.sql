f71842a5bc3a641640d6b83fa84e548237bd3f8e
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:02:04 +0530

    Add SQL query to select employee names

---
+++ basic/Employee_Names.sql
@@ -0,0 +1,8 @@
+-- Problem: Employee Names
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/name-of-employees/submissions/code/447227145
+
+SELECT name
+FROM employee   
+ORDER BY name ASC;
