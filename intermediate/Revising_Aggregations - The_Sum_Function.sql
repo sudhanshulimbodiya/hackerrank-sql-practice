92d9d6226a1619c40a22ebf37c9c8c0da3caf7f6
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:14:36 +0530

    Create SQL file for sum of California population
    Add SQL query to calculate total population in California

---
+++ intermediate/Revising_Aggregations - The_Sum_Function.sql
@@ -0,0 +1,8 @@
+-- Problem: Revising Aggregations - The Sum Function
+-- Difficulty: Intermediate
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/revising-aggregations-sum/submissions/code/452912229
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'CALIFORNIA';
