7a043fef48471142d104ce656d38276c0dae752f
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:23:34 +0530

    Create Population_Density_Difference.sql
    Add SQL query to calculate population density difference

---
+++ intermediate/Population_Density_Difference.sql
@@ -0,0 +1,7 @@
+-- Problem: Population Density Difference
+-- Difficulty: Intermediate 
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/population-density-difference/submissions/code/452831240
+
+SELECT MAX(POPULATION)-MIN(POPULATION)
+FROM CITY;
