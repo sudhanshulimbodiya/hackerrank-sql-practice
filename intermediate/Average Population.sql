8f1a97107b4d688c2deef0ea95c5e065ab080459
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:19:01 +0530

    Create Average Population SQL query
    Add SQL query to calculate average population from CITY table.

---
+++ intermediate/Average Population.sql
@@ -0,0 +1,7 @@
+-- Problem: Average Population
+-- Difficulty: Intermediate 
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/average-population/submissions/code/452912557
+
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
