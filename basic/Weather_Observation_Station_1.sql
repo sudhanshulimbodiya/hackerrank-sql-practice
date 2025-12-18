e9c2d088ca3006d9822885153dde64af6b77e16b
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 17:21:08 +0530

    Create Weather_Observation_Station_1.sql

---
+++ basic/Weather_Observation_Station_1.sql
@@ -0,0 +1,7 @@
+-- Problem: Select All
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-1/submissions/code/440568236
+
+SELECT CITY, STATE
+FROM STATION;
