cc766599d89d22aa8a7857dd08c2a6a64666e0c5
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 17:16:53 +0530

    Create SQL file for Japanese Cities' Attributes
    Add SQL query to select attributes of Japanese cities.

---
+++ basic/Japanese_Cities’_Attributes.sql
@@ -0,0 +1,8 @@
+-- Problem: Japanese Cities' Attributes
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/submissions/code/440565657
+
+SELECT *
+FROM city 
+WHERE countrycode = 'JPN'
