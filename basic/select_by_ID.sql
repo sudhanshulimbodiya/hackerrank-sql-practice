bed3271082616a352abc09e61058ac36eae8e9f5
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 17:13:51 +0530

    Create select_by_ID.sql for city selection
    Add SQL query to select a city by ID.

---
+++ basic/select_by_ID.sql
@@ -0,0 +1,8 @@
+-- Problem: Select by ID
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/select-by-id/submissions/code/440565452
+
+SELECT *
+FROM city
+WHERE id = '1661'
