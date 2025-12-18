5a8eccbcbc8c45a95091e7b61d684bbcdafb54be
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:41:01 +0530

    Create SQL query for Weather Observation Station 18
    Add SQL query for Weather Observation Station 18.

---
+++ intermediate/Weather_Observation_Station_18.sql
@@ -0,0 +1,8 @@
+-- Problem: Weather Observation Station 18
+-- Difficulty: Intermediate 
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-18/submissions/code/453102467
+
+SELECT 
+    ROUND(abs(MIN(LAT_N) - MAX(LAT_N)) + abs(MIN(LONG_W) - MAX(LONG_W)),4)
+FROM STATION ;
