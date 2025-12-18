b3e02415ff18e2e2f6e5cc1f5906e4c637728916
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:12:32 +0530

    Add SQL query for counting districts
    Add SQL query to count districts with population over 100,000.

---
+++ intermediate/Revising_Aggregations - The_Count_Function.sql
@@ -0,0 +1,8 @@
+-- Problem: Revising Aggregations - The Count Function
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/submissions/code/452912108
+
+SELECT COUNT(DISTRICT)
+FROM CITY
+WHERE POPULATION>100000;
