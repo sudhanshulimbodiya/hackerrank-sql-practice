6f57bea458d3584b1ea4e8238f9ce3a45a23e44b
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 22:58:53 +0530

    Update SQL query to select distinct cities

---
+++ basic/Weather_Observation_Station_12.sql
@@ -0,0 +1,17 @@
+-- Problem: Select All
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-12/submissions/code/441441984
+
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE (CITY NOT LIKE 'a%' AND
+       CITY NOT LIKE 'e%' AND
+       CITY NOT LIKE 'i%' AND
+       CITY NOT LIKE 'o%' AND
+       CITY NOT LIKE 'u%') AND 
+      (CITY NOT LIKE '%a' AND
+       CITY NOT LIKE '%e' AND
+       CITY NOT LIKE '%i' AND
+       CITY NOT LIKE '%o' AND
+       CITY NOT LIKE '%u');
