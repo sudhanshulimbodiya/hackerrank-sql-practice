c40af19ef37cb78dba2860f56dc91a81b4021423
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 17:10:19 +0530

    Add select_all.sql for basic SQL challenge
    Added SQL query to select all records from the CITY table.

---
+++ basic/select_all.sql
@@ -0,0 +1,7 @@
+-- Problem: Select All
+-- Difficulty: Basic
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/select-all-sql/submissions/code/457585784
+
+SELECT *
+FROM CITY;
