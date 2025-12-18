b3ceba353cf1f2ad9696a6466069e78a481943d6
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-18 23:29:17 +0530

    Create Weather_Observation_Station_2.sql with query
    Add SQL query for Weather Observation Station 2 problem

---
+++ intermediate/Weather_Observation_Station_2.sql
@@ -0,0 +1,7 @@
+-- Problem: Weather Observation Station 2
+-- Difficulty: Intermediate 
+-- Platform: HackerRank
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-2/submissions/code/452918861
+
+SELECT ROUND(SUM(LAT_N),2) AS lat, ROUnD(SUM(LONG_W),2) AS lon
+FROM STATION ;
