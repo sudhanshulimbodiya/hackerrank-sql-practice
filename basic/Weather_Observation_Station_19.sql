499bb0b6db6d959d1b2bcde29294d75911d730f6
Sudhanshu Limbodiya <limbodiyasudhanshu@gmail.com>
2025-12-19 18:26:08 +0530

    Create SQL script for Weather Observation Station 19

--- basic/Weather_Observation_Station_19.sql
+++ basic/Weather_Observation_Station_19.sql
@@ -1,5 +1,5 @@
 -- Problem: Weather Observation Station 19
--- Difficulty: Intermediate 
+-- Difficulty: Basic 
 -- Platform: HackerRank
 -- Link: https://www.hackerrank.com/challenges/weather-observation-station-19/submissions/code/453103752
 
